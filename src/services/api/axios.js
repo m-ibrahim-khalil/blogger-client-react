15405b30a43320914c1f87e72aa7f09f5b32dd7a
bsse1009 <mik858692@gmail.com>
2023-05-15 20:47:13 +0600

    added redirects

--- src/services/api/axios.js
+++ src/services/api/axios.js
@@ -4,7 +4,6 @@ import axios from 'axios';
 // const API_BASE_URL = 'http://localhost:3001/api/v1';
 
 const Axios = axios.create({
-  // baseURL: API_BASE_URL,
   timeout: 5000,
   withCredentials: true,
   headers: {
