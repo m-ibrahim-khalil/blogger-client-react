ef1c24b273d5ffa042ca7eebdf3a8b8cc2d82395
bsse1009 <mik858692@gmail.com>
2023-05-15 18:00:03 +0600

    modified vite.cong

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,15 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    // proxy: {
-    //   '/api': 'https://fine-lime-bull-gear.cyclic.app/',
-    //   '.*\/api.*': {
-    //     target: 'https://fine-lime-bull-gear.cyclic.app/',
-    //     changeOrigin: true,
-    //   },
-    //   'https://musical-dolphin-612496.netlify.app/api': 'https://fine-lime-bull-gear.cyclic.app/',
-    // },
+    proxy: {
+      '/api': 'https://fine-lime-bull-gear.cyclic.app/',
+      // '.*/api.*': {
+      //   target: 'https://fine-lime-bull-gear.cyclic.app/',
+      //   changeOrigin: true,
+      // },
+      // 'https://musical-dolphin-612496.netlify.app/api':
+      //   'https://fine-lime-bull-gear.cyclic.app/',
+    },
   },
   plugins: [react()],
 });
